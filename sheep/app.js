5530ac2a3c8da72bf5668c94435ee0fc9ea9e660
kboysm <lishal12@naver.com>
2021-03-26 04:55:31 +0900

    모르는 함수 분석 후 주석처리

--- sheep/app.js
+++ sheep/app.js
@@ -12,17 +12,18 @@ class App {
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
-        requestAnimationFrame(this.animate.bind(this));
+        requestAnimationFrame(this.animate.bind(this)); // 비동기 , 브라우저가 실행 시기를 결정 , 모니터 주사율에 맞춰 함수를 실행
+        //웹 브라우저에 프레임이라는 개념을 넣음 , setInterval 을 대체 , 성능이 상당히 좋음 , 재귀함수
     }
 
     resize() {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
 
-        this.canvas.width = this. stageWidth * 2;
-        this.canvas.height = this. stageHeight * 2;
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
-
+        //The CanvasRenderingContext2D.scale() method of the Canvas 2D API adds a scaling transformation to the canvas units horizontally and/or vertically.
         for (let i= 0; i< this.hills.length; i++) {
             this.hills[i].resize(this.stageWidth, this.stageHeight);
         }
@@ -32,7 +33,7 @@ class App {
         requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-
+        //The CanvasRenderingContext2D.clearRect() method of the Canvas 2D API erases the pixels in a rectangular area by setting them to transparent black.
         let dots;
         for (let i =0; i < this.hills.length; i++) {
             dots = this.hills[i].draw(this.ctx);
--- sheep/hill.js
+++ sheep/hill.js
@@ -23,13 +23,14 @@ export class Hill {
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.beginPath();
-
+        //The CanvasRenderingContext2D.beginPath() method of the Canvas 2D API starts a new path by emptying the list of sub-paths. Call this method when you want to create a new path.
+        // 경로를 다시 그려줌
         let cur = this.points[0];
         let prev = cur;
         let dots = [];
 
         ctx.moveTo(cur.x, cur.y);
-
+        // 선 그리기 시작 좌표 함수 , 선 그리기 위치로 이동
         let prevCx = cur.x;
         let prevCy = cur.y;
 
@@ -38,7 +39,7 @@ export class Hill {
 
             const cx = (prev.x + cur.x) / 2;
             const cy = (prev.y + cur.y) / 2;
-            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy); // 시작점 , 종료점 사이에 제어점 좌표를 입력하고 시작점과 종료점의 선을 곡선으로
 
             dots.push({
                 x1: prevCx,
@@ -55,10 +56,10 @@ export class Hill {
 
         }
 
-        ctx.lineTo(prev.x, prev.y);
+        ctx.lineTo(prev.x, prev.y); // 선 그리기 종료 좌표 함수, 좌표 위치까지 선 그리기
         ctx.lineTo(this.stageWidth, this.stageHeight);
         ctx.lineTo(this.points[0].x, this.stageHeight);
-        ctx.fill();
+        ctx.fill(); // 지정된 색을 넣어줌
 
         return dots;
     }
